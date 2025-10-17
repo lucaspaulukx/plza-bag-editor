30aa3f79bd188f2b328770ee1be1aff7e59d54dc
azalea <238357242+azalea-w@users.noreply.github.com>
2025-10-17 19:04:23 +0200

    Update output method to allow scripting

--- main.py
+++ main.py
@@ -1,62 +1,72 @@
+import json
 import os
 import sys
 
 from lib.plaza.crypto import HashDB, SwishCrypto
-from lib.plaza.types import BagSave, BagEntry, CategoryType
+from lib.plaza.types import BagEntry, BagSave, CategoryType
 from lib.plaza.util.items import item_db
-
 
 save_file_magic = bytes([
 	0x17, 0x2D, 0xBB, 0x06, 0xEA
 ])
 
-
 if __name__ == "__main__":
+    output_normally = True
     # noinspection DuplicatedCode
     if not sys.argv[1:]:
         print("Usage: python main.py <path_to_file>")
         sys.exit(1)
 
     file_path = sys.argv[1]
-    print(F"PLZA Save Repair Script")
-    print(f"File path: {file_path}")
+    if len(sys.argv) > 2:
+        output_normally = False
+
+    def log(message: str, _data = None):
+        if not _data:
+            _data = {}
+
+        if output_normally: return print(message)
+        elif not output_normally and _data: return print(json.dumps(_data | {"log": message}, indent=4))
+
+    log(f"PLZA Save Repair Script")
+    log(f"File path: {file_path}")
 
     if not os.path.exists(file_path):
-        print(f"File not found: {file_path}")
+        log(f"File not found: {file_path}", {"success": False})
         sys.exit(1)
 
     with open(file_path, "rb") as f:
         data = f.read()
 
     if not data.startswith(save_file_magic):
-        print("File is not a PLZA save file")
+        log("File is not a PLZA save file", {"success": False})
         sys.exit(1)
 
     try:
         blocks = SwishCrypto.decrypt(data)
     except Exception as e:
-        print(f"Error decrypting save file: {e}")
+        log(f"Error decrypting save file: {e}", {"success": False})
         sys.exit(1)
 
-    print(f"Decrypted {len(blocks)} Blocks.")
-    print(f"{SwishCrypto.get_is_hash_valid(data)=}")
+    log(f"Decrypted {len(blocks)} Blocks.")
+    log(f"{SwishCrypto.get_is_hash_valid(data)=}")
     hash_db = HashDB(blocks)
     try:
         bag_save_index = 0x21C9BD44
         bag_save = hash_db[bag_save_index]
     except KeyError as e:
-        print("BagSave index not found")
+        log("BagSave index not found", {"success": False})
         sys.exit(1)
 
     if len(bag_save.data) != 48128:
-        print("Invalid bag size, can't fix!")
+        log("Invalid bag size, can't fix!", {"success": False})
         sys.exit(1)
 
     parsed_bag_save = BagSave.from_bytes(bag_save.data)
 
-    print(f"Parsed BagSave: {parsed_bag_save}")
+    log(f"Parsed BagSave: {parsed_bag_save}")
 
-    print(parsed_bag_save)
+    log(parsed_bag_save)
 
     edited_count = 0
     for i, entry in enumerate(parsed_bag_save.entries):
@@ -64,31 +74,30 @@ if __name__ == "__main__":
 
         # * Category < 0 causes crash
         if entry.category.value < 0:
-            print(f"Item with corrupt category encountered")
+            log(f"Item with corrupt category encountered")
             if i in item_db and not item_db[i]["canonical_name"].endswith("NAITO"):
-                print(f"Restored {item_db[i]['english_ui_name']}")
+                log(f"Restored {item_db[i]['english_ui_name']}")
                 entry.category = item_db[i]["expected_category"].value
             else:
                 entry.quantity = 0
             parsed_bag_save.set_entry(i, BagEntry.from_bytes(entry.to_bytes()))
             edited_count += 1
-            continue
 
         # * Item is not used
         if not i in item_db:
-            print(f"Removing item at index {i}")
+            log(f"Removing item at index {i}")
             entry.quantity = 0
+            entry.category = 0
             parsed_bag_save.set_entry(i, BagEntry.from_bytes(entry.to_bytes()))
             edited_count += 1
             continue
 
         # * Item has wrong category
         if entry.category != item_db[i]["expected_category"]:
-            print(f"Editing category of {item_db[i]['english_ui_name']} ({entry.category} -> {item_db[i]['expected_category']})")
+            log(f"Editing category of {item_db[i]['english_ui_name']} ({entry.category} -> {item_db[i]['expected_category']})")
             entry.category = item_db[i]["expected_category"].value
             parsed_bag_save.set_entry(i, BagEntry.from_bytes(entry.to_bytes()))
             edited_count += 1
-            continue
 
         # * Mega Stone Quantity Check
         if (
@@ -96,24 +105,23 @@ if __name__ == "__main__":
             and item_db[i]["canonical_name"].strip("xy").endswith("NAITO")
             and entry.quantity > 1
         ):
-            print(f"Editing quantity of {item_db[i]['english_ui_name']}")
+            log(f"Editing quantity of {item_db[i]['english_ui_name']}")
             entry.quantity = 1
             parsed_bag_save.set_entry(i, BagEntry.from_bytes(entry.to_bytes()))
             edited_count += 1
-            continue
 
     if not edited_count:
-        print("No items needed to be modified!")
+        log("No items needed to be modified!", {"success": True})
         sys.exit(0)
 
-    print(f"Done! Modified {edited_count} entries")
+    log(f"Done! Modified {edited_count} entries", {"edited_count": edited_count, "success": True})
 
     hash_db[bag_save_index].change_data(parsed_bag_save.to_bytes())
 
     out = SwishCrypto.encrypt(hash_db.blocks)
-    print(f"Writing Modified file to {file_path}_modified")
+    log(f"Writing Modified file to {file_path}_modified")
 
     with open(file_path + "_modified", "wb") as f:
         f.write(out)
 
-    print(f"Wrote File, Exiting")
+    log(f"Wrote File, Exiting")
